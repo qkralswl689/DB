a2fbc0a6078949ef565e15d2c7430e98b6431ff2
qkralswl689 <65608960+qkralswl689@users.noreply.github.com>
2021-03-23 18:09:06 +0900

    03.23

---
+++ MariaDB/03_23_테이블생성및삽입.sql
@@ -0,0 +1,14 @@
+-- 사용할 db명시
+USE bigdb; 
+
+-- 테이블 생성
+CREATE TABLE temp (
+	id VARCHAR(20) PRIMARY KEY,
+	NAME VARCHAR(100) NOT NULL
+);
+
+-- 데이터 삽입
+INSERT INTO temp VALUES('hyun','혀니');
+
+-- 정보 검색
+SELECT * FROM temp;
