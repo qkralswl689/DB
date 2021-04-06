b9e6abefd28da7ce4f8c75a8c127b5f05be9df89
qkralswl689 <65608960+qkralswl689@users.noreply.github.com>
2021-04-06 18:23:46 +0900

    04.06

---
+++ SQL/mingki/04_06_newlecture.sql
@@ -0,0 +1,42 @@
+CREATE TABLE NOTICE
+(
+     ID		NUMBER,
+     TITLE 		NVARCHAR2(100),
+     WRITER_ID	NVARCHAR2(50),
+     CONTENT	CLOB,
+     REGDATE  	TIMESTAMP,
+     HIT		NUMBER,
+     FILES		NVARCHAR2(1000)
+);
+
+CREATE TABLE "COMMENT"
+(
+     ID		NUMBER,
+     CONTENT	NVARCHAR2(2000),
+     REGDATE  	TIMESTAMP,
+     WRITER_ID	NVARCHAR2(50),
+     NOTICE_ID	NUMBER
+);
+
+CREATE TABLE ROLE
+(
+     ID		VARCHAR2(50),
+     DISCRIPTION 	NVARCHAR2(500)
+);
+
+CREATE TABLE MEMBER_ROLE
+(
+     MEMBER_ID	NVARCHAR2(50),
+     ROLE_ID		VARCHAR2(50)
+);
+-- 데이터 넣기
+insert into notice values
+(
+    123,
+     '안녕',
+    '배달팽',
+    '안녕하세여',
+   current_timestamp,
+     1,
+     '190입니다'
+);
