ce125ed7a1899008c892ab36c901fb9b765fa93a
qkralswl689 <65608960+qkralswl689@users.noreply.github.com>
2021-04-06 22:38:37 +0900

    Update 04_06_newlecture.sql

--- SQL/mingki/04_06_newlecture.sql
+++ SQL/mingki/04_06_newlecture.sql
@@ -40,3 +40,47 @@ insert into notice values
      1,
      '190입니다'
 );
+commit;
+insert into notice values
+(
+    456,
+     '안녕1',
+    '배달팽2',
+    '안녕하세여3',
+   current_timestamp,
+     14,
+     '190입니다'
+);
+commit;
+insert into notice values
+(
+    1234,
+     '안녕3',
+    '배달팽3',
+    '안녕하세여3',
+   current_timestamp,
+     14,
+     '190입니다'
+);
+commit;
+insert into notice values
+(
+    1235,
+     '안녕5',
+    '배달팽5',
+    '안녕하세여5',
+   current_timestamp,
+     15,
+     '199입니다'
+);
+commit;
+
+insert into notice values(1, 'JDBC란 무엇인가?', 'okay', 'aaa', sysdate, 1, '');
+insert into notice values(2, 'JDBC 드라이버 다운로드 방법', 'newlec', 'aaa', sysdate, 10, '');
+insert into notice values(3, '전화주시기 바랍니다.', 'newlec', '연락처 남깁니다. 010-2222-2333', sysdate, 22, '');
+insert into notice values(4, 'Service 계층 추가하기', 'dragon', 'aaa', sysdate, 1, '');
+insert into notice values(5, 'JSP에서 JDBC 사용하기', 'newlec', 'soso', sysdate, 33, '');
+insert into notice values(6, '파라미터를 가지는 문장 실행하기', 'okay', 'aaa', sysdate, 1, '');
+insert into notice values(7, '추가요', 'dragon', 'aaa', sysdate, 44, '');
+insert into notice values(8, '디엔드', 'okay', 'aaa', sysdate, 55, '');
+commit;
